6f980e65311873ce2a3fbd831645095723657477
jmarcossss <joaomalcantaraaa@gmail.com>
2023-05-02 15:41:47 -0300

    fix(*): fixing error hub repository

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -5,38 +5,42 @@ function log(message: string) {
   console.log(`[LOG] ${message}`);
 }
 
-// Função genérica para buscar uma reserva
-async function findBooking(options: { userId?: number; roomId?: number }) {
-  log('Iniciando a função findBooking');
-  const result = await prisma.booking.findFirst({ where: options });
-  log('Finalizando a função findBooking');
-  return result;
-}
-
 // Função para buscar uma reserva por ID do usuário
 async function findBookingByUserId(userId: number) {
-  return findBooking({ userId });
+  log('Iniciando a função findBookingByUserId');
+  const result = await prisma.booking.findFirst({
+    where: { userId: userId },
+  });
+  log('Finalizando a função findBookingByUserId');
+  return result;
 }
 
 // Função para buscar uma reserva por ID do quarto
 async function findBookingByRoomId(roomId: number) {
-  return findBooking({ roomId });
+  log('Iniciando a função findBookingByRoomId');
+  const result = await prisma.booking.findFirst({
+    where: { roomId: roomId },
+  });
+  log('Finalizando a função findBookingByRoomId');
+  return result;
 }
 
 // Função genérica para criar uma reserva
-async function createBooking(options: { roomId: number; userId: number }) {
+async function createBooking(roomId: number, userId: number) {
   log('Iniciando a função createBooking');
-  const result = await prisma.booking.create({ data: options });
+  const result = await prisma.booking.create({
+    data: { roomId: roomId, userId: userId },
+  });
   log('Finalizando a função createBooking');
   return result;
 }
 
 // Função para atualizar uma reserva por ID
-async function updateBookingById(bookingId: number, options: { roomId?: number }) {
+async function updateBookingById(bookingId: number, roomId: number) {
   log('Iniciando a função updateBookingById');
   const result = await prisma.booking.update({
     where: { id: bookingId },
-    data: options,
+    data: { roomId: roomId },
   });
   log('Finalizando a função updateBookingById');
   return result;
