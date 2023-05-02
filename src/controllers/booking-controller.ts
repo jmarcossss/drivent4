5b3655f16d7e3c5966bbabf979080a32ec16dd41
jmarcossss <joaomalcantaraaa@gmail.com>
2023-05-01 22:29:26 -0300

    fix(controller): fixing booking controller error

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -4,72 +4,53 @@ import { AuthenticatedRequest } from '@/middlewares';
 import bookingService from '@/services/booking-service';
 import hotelRepository from '@/repositories/hotel-repository';
 
-export async function findBooking(
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction,
-): Promise<Response<any, Record<string, any>> | void> {
+export async function findBooking(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
   const { userId } = req;
   try {
     const booking = await bookingService.findBooking(userId);
     if (!booking) {
-      return res.status(httpStatus.NOT_FOUND).send({
-        error: 'Booking not found',
-      });
+      throw new Error('Booking not found');
     }
     const room = await hotelRepository.findRoomById(booking.roomId);
     if (!room) {
-      return res.status(httpStatus.NOT_FOUND).send({
-        error: 'Room not found',
-      });
+      throw new Error('Room not found');
     }
     return res.status(httpStatus.OK).send({
       id: booking.id,
       Room: { ...room },
     });
   } catch (e) {
-    return next(e);
+    next(e);
   }
 }
 
-export async function createBooking(
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction,
-): Promise<Response<any, Record<string, any>> | void> {
+export async function createBooking(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
   const { userId } = req;
   const { roomId } = req.body as { roomId: number };
   try {
     const booking = await bookingService.createBooking(roomId, userId);
     if (!booking) {
-      return res.status(httpStatus.BAD_REQUEST).send({
-        error: 'Unable to create booking',
-      });
+      throw new Error('Booking creation failed');
     }
     return res.status(httpStatus.OK).send({ bookingId: booking.bookingId });
   } catch (e) {
-    return next(e);
+    next(e);
   }
 }
 
-export async function updateBooking(
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction,
-): Promise<Response<any, Record<string, any>> | void> {
+export async function updateBooking(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
   const { userId } = req;
-  const { roomId, bookingId } = req.body as { roomId: number; bookingId: number };
+  const { roomId } = req.body as { roomId: number };
+  const { bookingId } = req.params;
   try {
-    const booking = await bookingService.updateBooking(roomId, userId, bookingId);
+    const booking = await bookingService.updateBooking(roomId, userId, +bookingId);
     if (!booking) {
-      return res.status(httpStatus.BAD_REQUEST).send({
-        error: 'Unable to update booking',
-      });
+      throw new Error('Booking update failed');
     }
     return res.status(httpStatus.OK).send({
       bookingId: booking.id,
     });
   } catch (e) {
-    return next(e);
+    next(e);
   }
 }
---
+++ src/repositories/booking-repository/index-teste.ts
@@ -0,0 +1,60 @@
+// import { prisma } from '@/config';
+
+// async function findBookingByUserId(userId: number) {
+//   const booking = await prisma.booking.findFirst({
+//     where: {
+//       userId,
+//     },
+//   });
+
+//   if (!booking) {
+//     throw new Error('Booking not found');
+//   }
+
+//   return booking;
+// }
+
+// async function findBookingByRoomId(roomId: number) {
+//   const booking = await prisma.booking.findFirst({
+//     where: {
+//       roomId,
+//     },
+//   });
+
+//   if (!booking) {
+//     throw new Error('Booking not found');
+//   }
+
+//   return booking;
+// }
+
+// async function createBooking(roomId: number, userId: number) {
+//   const booking = await prisma.booking.create({
+//     data: {
+//       roomId,
+//       userId,
+//     },
+//   });
+
+//   return booking;
+// }
+
+// async function updateBookingById(bookingId: number, roomId: number) {
+//   const booking = await prisma.booking.update({
+//     where: {
+//       id: bookingId,
+//     },
+//     data: {
+//       roomId,
+//     },
+//   });
+
+//   return booking;
+// }
+
+// export default {
+//   findBookingByUserId,
+//   findBookingByRoomId,
+//   createBooking,
+//   updateBookingById,
+// };
