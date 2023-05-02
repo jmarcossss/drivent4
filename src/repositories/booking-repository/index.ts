be403963396e0a8746894e91ba0043b1e9ecdf07
jmarcossss <joaomalcantaraaa@gmail.com>
2023-05-01 23:34:35 -0300

    feat(repository: adding booking repository

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -1,39 +1,55 @@
 import { prisma } from '@/config';
 
 async function findBookingByUserId(userId: number) {
-  return await prisma.booking.findFirst({
+  const booking = await prisma.booking.findFirst({
     where: {
-      userId: userId,
+      userId,
     },
   });
+
+  if (!booking) {
+    throw new Error('Booking not found');
+  }
+
+  return booking;
 }
 
 async function findBookingByRoomId(roomId: number) {
-  return await prisma.booking.findFirst({
+  const booking = await prisma.booking.findFirst({
     where: {
-      roomId: roomId,
+      roomId,
     },
   });
+
+  if (!booking) {
+    throw new Error('Booking not found');
+  }
+
+  return booking;
 }
 
 async function createBooking(roomId: number, userId: number) {
-  return await prisma.booking.create({
+  const booking = await prisma.booking.create({
     data: {
-      roomId: roomId,
-      userId: userId,
+      roomId,
+      userId,
     },
   });
+
+  return booking;
 }
 
 async function updateBookingById(bookingId: number, roomId: number) {
-  return await prisma.booking.update({
+  const booking = await prisma.booking.update({
     where: {
       id: bookingId,
     },
     data: {
-      roomId: roomId,
+      roomId,
     },
   });
+
+  return booking;
 }
 
 export default {
--- src/schemas/booking-schemas.ts
+++ src/schemas/booking-schemas.ts
@@ -1,10 +1,5 @@
 import Joi from 'joi';
 import { Booking } from '@prisma/client';
 
-export const bookingSchema = Joi.object<Booking>({
-  roomId: Joi.number().required(),
-});
-
-export const bookingSchemaUpdate = Joi.object<Booking>({
-  roomId: Joi.number(),
-});
+export const bookingSchema = Joi.object<Booking>({ roomId: Joi.number().required() });
+export const bookingSchemaUpdate = Joi.object<Booking>({ roomId: Joi.number() });
