177f6058440169bd8f6c2975c27fc31366452850
jmarcossss <joaomalcantaraaa@gmail.com>
2023-05-02 00:22:05 -0300

    chore(*): resolving hub error

--- src/repositories/booking-repository/index-teste.ts
+++ src/repositories/booking-repository/index-teste.ts
@@ -1,55 +1,56 @@
 // import { prisma } from '@/config';
 
+// // Função de log
+// function log(message: string) {
+//   console.log(`[LOG] ${message}`);
+// }
+
 // async function findBookingByUserId(userId: number) {
-//   const booking = await prisma.booking.findFirst({
+//   log('Iniciando a função findBookingByUserId');
+//   const result = await prisma.booking.findFirst({
 //     where: {
-//       userId,
+//       userId: userId,
 //     },
 //   });
-
-//   if (!booking) {
-//     throw new Error('Booking not found');
-//   }
-
-//   return booking;
+//   log('Finalizando a função findBookingByUserId');
+//   return result;
 // }
 
 // async function findBookingByRoomId(roomId: number) {
-//   const booking = await prisma.booking.findFirst({
+//   log('Iniciando a função findBookingByRoomId');
+//   const result = await prisma.booking.findFirst({
 //     where: {
-//       roomId,
+//       roomId: roomId,
 //     },
 //   });
-
-//   if (!booking) {
-//     throw new Error('Booking not found');
-//   }
-
-//   return booking;
+//   log('Finalizando a função findBookingByRoomId');
+//   return result;
 // }
 
 // async function createBooking(roomId: number, userId: number) {
-//   const booking = await prisma.booking.create({
+//   log('Iniciando a função createBooking');
+//   const result = await prisma.booking.create({
 //     data: {
-//       roomId,
-//       userId,
+//       roomId: roomId,
+//       userId: userId,
 //     },
 //   });
-
-//   return booking;
+//   log('Finalizando a função createBooking');
+//   return result;
 // }
 
 // async function updateBookingById(bookingId: number, roomId: number) {
-//   const booking = await prisma.booking.update({
+//   log('Iniciando a função updateBookingById');
+//   const result = await prisma.booking.update({
 //     where: {
 //       id: bookingId,
 //     },
 //     data: {
-//       roomId,
+//       roomId: roomId,
 //     },
 //   });
-
-//   return booking;
+//   log('Finalizando a função updateBookingById');
+//   return result;
 // }
 
 // export default {
--- src/schemas/booking-schemas.ts
+++ src/schemas/booking-schemas.ts
@@ -2,4 +2,12 @@ import Joi from 'joi';
 import { Booking } from '@prisma/client';
 
 export const bookingSchema = Joi.object<Booking>({ roomId: Joi.number().required() });
+
+function obterBooking(booking: any, schema: Joi.ObjectSchema<Booking>) {
+  const { error } = schema.validate(booking);
+  if (error) {
+    throw new Error(`Invalid booking data: ${error.message}`);
+  }
+}
+
 export const bookingSchemaUpdate = Joi.object<Booking>({ roomId: Joi.number() });
