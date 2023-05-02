7c063351ddc94aa002ac2f0e3c257a27953cac0e
jmarcossss <joaomalcantaraaa@gmail.com>
2023-05-01 21:48:31 -0300

    feat(controller): adding booking controller

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -4,42 +4,72 @@ import { AuthenticatedRequest } from '@/middlewares';
 import bookingService from '@/services/booking-service';
 import hotelRepository from '@/repositories/hotel-repository';
 
-export async function findBooking(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
+export async function findBooking(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<Response<any, Record<string, any>> | void> {
   const { userId } = req;
   try {
     const booking = await bookingService.findBooking(userId);
+    if (!booking) {
+      return res.status(httpStatus.NOT_FOUND).send({
+        error: 'Booking not found',
+      });
+    }
     const room = await hotelRepository.findRoomById(booking.roomId);
-
+    if (!room) {
+      return res.status(httpStatus.NOT_FOUND).send({
+        error: 'Room not found',
+      });
+    }
     return res.status(httpStatus.OK).send({
       id: booking.id,
       Room: { ...room },
     });
   } catch (e) {
-    next(e);
+    return next(e);
   }
 }
 
-export async function createBooking(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
+export async function createBooking(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<Response<any, Record<string, any>> | void> {
   const { userId } = req;
   const { roomId } = req.body as { roomId: number };
   try {
-    const { bookingId } = await bookingService.createBooking(roomId, userId);
-    return res.status(httpStatus.OK).send({ bookingId });
+    const booking = await bookingService.createBooking(roomId, userId);
+    if (!booking) {
+      return res.status(httpStatus.BAD_REQUEST).send({
+        error: 'Unable to create booking',
+      });
+    }
+    return res.status(httpStatus.OK).send({ bookingId: booking.bookingId });
   } catch (e) {
-    next(e);
+    return next(e);
   }
 }
 
-export async function updateBooking(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<Response> {
+export async function updateBooking(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<Response<any, Record<string, any>> | void> {
   const { userId } = req;
-  const { roomId } = req.body as { roomId: number };
-  const { bookingId } = req.params;
+  const { roomId, bookingId } = req.body as { roomId: number; bookingId: number };
   try {
-    const { id: bookingIdFromUpdatedRoom } = await bookingService.updateBooking(roomId, userId, +bookingId);
+    const booking = await bookingService.updateBooking(roomId, userId, bookingId);
+    if (!booking) {
+      return res.status(httpStatus.BAD_REQUEST).send({
+        error: 'Unable to update booking',
+      });
+    }
     return res.status(httpStatus.OK).send({
-      bookingId: bookingIdFromUpdatedRoom,
+      bookingId: booking.id,
     });
   } catch (e) {
-    next(e);
+    return next(e);
   }
 }
